4f45d1fd5af548edeb7aa1ef059eb90fe4d0f865
Henry Bernardo <henrybernardolk@gmail.com>
2023-03-15 21:22:45 -0300

    add config/routes

---
+++ config/routes.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const routes = express.Router()
+
+let db = [
+  { 1: { Nome: 'Cliente 1', Idade: '20' } },
+  { 2: { Nome: 'Cliente 2', Idade: '20' } },
+  { 3: { Nome: 'Cliente 3', Idade: '20' } }
+]
+
+routes.get('/', (req, res) => {
+  return res.json(db)
+})
+
+routes.post('/add', (req, res) => {
+  const body = req.body
+
+  if (!body) return res.status(400).end()
+
+  db.push(body)
+  return res.json(body)
+})
+
+routes.delete('/:id', (req, res) => {
+  const id = req.params.id
+
+  let newDB = db.filter(item => {
+    if (!item[id]) return item
+  })
+
+  db = newDB
+
+  return res.send(newDB)
+})
+
+module.exports = routes
--- index.js
+++ index.js
@@ -3,31 +3,15 @@ const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const routes = require('./config/routes')
+
 const app = express()
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
-
-let db = [
-  { 1: { Nome: 'Cliente 1', Idade: '20' } },
-  { 2: { Nome: 'Cliente 2', Idade: '20' } },
-  { 3: { Nome: 'Cliente 3', Idade: '20' } }
-]
-
-app.get('/', (req, res) => {
-  return res.json(db)
-})
-
-app.post('/add', (req, res) => {
-  const body = req.body
-
-  if (!body) return res.status(400).end()
-
-  db.push(body)
-  return res.json(body)
-})
+app.use(routes)
 
 app.listen(3030, () => {
   console.log(`Express started at http://localhost:21262`)
